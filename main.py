443b92c6b5ce2c8e711ad2511d0afa39cb0e7cfc
lauren <pmd7211@gmail.com>
2026-04-25 16:13:27 -0400

    fixes

--- main.py
+++ main.py
@@ -165,10 +165,9 @@ def retrieve_sound_theta_and_r():
     # # except:
     # #     print("[ERROR] failed to retrieve sound data properly")
     # #     return 0, 800
-    # theta = int(input("Theta:"))
-    # r = int(input("R:"))
-    # return theta, r
-    return 0, 9000
+    theta = int(input("Theta:"))
+    r = int(input("R:"))
+    return theta, r
 
 def orient():
     # stop moving and take a reading
@@ -177,7 +176,7 @@ def orient():
     theta_to_turn, _ = retrieve_sound_theta_and_r()
     odometry_to_turn = (FULL_TURN_ODOMETRY / 360) * theta_to_turn
 
-    while 10 < abs(odometry_to_turn) < 350:
+    while 10 < abs(theta_to_turn) < 350:
         # perform turn
         read_all_encoder()
         initial = [x for x in encoder_now]
